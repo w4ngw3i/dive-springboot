c71f853b0d2deb483f89bfd6b8558d88fc10f009
wangwei <china.wangwei@icloud.com>
2018-11-12 15:31:19 +0800

    new file

---
+++ src/main/java/com/wangwei/divespringboot/DiveSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.wangwei.divespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiveSpringbootApplication.class, args);
+	}
+}
