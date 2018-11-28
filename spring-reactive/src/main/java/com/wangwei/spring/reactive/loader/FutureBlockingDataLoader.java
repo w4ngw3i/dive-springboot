1fd2c2187ed513baf782a827fcd15293c85f72fb
wangwei <china.wangwei@icloud.com>
2018-11-28 14:53:50 +0800

    add new module

---
+++ spring-reactive/src/main/java/com/wangwei/spring/reactive/loader/FutureBlockingDataLoader.java
@@ -0,0 +1,32 @@
+package com.wangwei.spring.reactive.loader;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * {@link Future} 阻塞数据加载器
+ * @Auther wangwei
+ * @Date 2018/11/26 4:21 PM
+ */
+public class FutureBlockingDataLoader extends DataLoader {
+
+    protected void doLoad() {
+        ExecutorService executorService = Executors.newFixedThreadPool(3); // 创建线程池
+        runCompletely(executorService.submit(super::loadConfigurations));
+        runCompletely(executorService.submit(super::loadUsers));
+        runCompletely(executorService.submit(super::loadOrders));
+        executorService.shutdown();
+    }
+
+    private void runCompletely(Future<?> future) {
+        try {
+            future.get();
+        } catch (Exception e) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new FutureBlockingDataLoader().load();
+    }
+}
